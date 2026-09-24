e21b19c268142a97f0535c89a87d8c2ddc866044
Yunice128 <yytan0202@gmail.com>
2026-09-24 09:07:32 +0800

    Create sw-v3.js

---
+++ sw-v3.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "grab-driver-net-3.0-v3";
+
+const FILES = [
+  "./",
+  "./index32.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
